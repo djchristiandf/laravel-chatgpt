865cb85ecb5c620c5f5fc888b2b91c5fd5db57d2
djchristiandf <djchristiandf@gmail.com>
2024-01-17 14:41:31 -0300

    sistema laravel-chagpt

---
+++ resources/js/Pages/Chat/ChatIndex.vue
@@ -0,0 +1,10 @@
+<script setup>
+import ChatLayout from '@/Layouts/ChatLayout.vue';
+
+</script>
+
+<template>
+    <ChatLayout>
+        <!-- This example requires Tailwind CSS v2.0+ -->
+    </ChatLayout>
+</template>
